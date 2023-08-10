070cf428fae8073245f7700285778dfcf729739c
Paulinha <paulafabianasoares@gmail.com>
2023-08-10 14:46:25 -0300

    chore: add chakraui, emotion, framer-emotion

---
+++ components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react'
+
+export const B = () => {
+  return <div>
+    
+    Button
+    <Button colorScheme='blue'>Button</Button>
+  </div>;
+};
+
